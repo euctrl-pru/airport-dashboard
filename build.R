ec02c2ce33f747fdd97b7adf50bb3dbb09e4d8e3
SPINIELLI Enrico <enrico.spinielli@eurocontrol.int>
2021-04-01 16:40:43 +0200

    build sequence

--- build.R
+++ build.R
@@ -1,12 +1,12 @@
 library(here)
 
 # export the data to CSV from DB
-# COMMENT OUT if already DONE
-source(here("R","apt_dshbd_get_data.R"), encoding = "UTF8")
+# UNCOMMENT  if data needs to be refreshed
+# source(here("R","apt_dshbd_get_data.R"), encoding = "UTF8")
 
 # generate airport layout
-# COMMENT OUT if no new airports have been added
-source(here("R","generate-apt-layout.R"), encoding = "UTF8")
+# UNCOMMENT  if new airports have been added
+# source(here("R","generate-apt-layout.R"), encoding = "UTF8")
 
 
 source(here("R","apt_dshbd_render.R"), encoding = "UTF8")
