7e78f0488cab7eaad903f4afac71373a8b860d07
SPINIELLI Enrico <enrico.spinielli@eurocontrol.int>
2021-07-14 13:17:06 +0200

    remove debug line

--- build.R
+++ build.R
@@ -2,7 +2,7 @@ library(here)
 
 # export the data to CSV from DB
 # UNCOMMENT  if data needs to be refreshed
-# source(here("R","apt_dshbd_get_data.R"), encoding = "UTF8")
+source(here("R","apt_dshbd_get_data.R"), encoding = "UTF8")
 
 # generate airport layout
 # UNCOMMENT  if new airports have been added
