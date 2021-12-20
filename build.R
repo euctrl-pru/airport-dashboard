dca13d53acbe9e4bc8e280fc748474597f9e2cd5
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2021-12-20 14:05:23 +0100

    test for Netlify

--- build.R
+++ build.R
@@ -10,4 +10,9 @@ source(here("R","apt_dshbd_get_data.R"), encoding = "UTF8")
 
 
 source(here("R","apt_dshbd_render.R"), encoding = "UTF8")
-source(here("R","apt_dshbd_index.R"), encoding = "UTF8")
+# source(here("R","apt_dshbd_index.R"), encoding = "UTF8")
+
+# regenerate index.html
+fs::file_copy(here("index.Rmd"), here("docs/"), overwrite = TRUE)
+rmarkdown::render(here("docs", "index.Rmd"),  encoding = 'UTF-8')
+fs::file_delete(path = here("docs", "index.Rmd"))
