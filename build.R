ba145db0b53029d7a5d5868da79d93060a646e5e
DE LANGE Thierry <thierry.delange@eurocontrol.int>
2022-01-17 10:35:24 +0100

    Data Refresh and HTML's regenerating

--- build.R
+++ build.R
@@ -1,11 +1,14 @@
+library(dplyr)
 library(here)
+library(stringr)
 
 # 0. cleanup: remove everything but (favicons and) `images/` 
 fs::dir_delete(here("docs", "libs"))
 fs::dir_ls(here("docs"), recurse = TRUE) %>%
   as_tibble() %>%
   filter(str_detect(value, pattern = "images", negate = TRUE),
-         str_detect(value, pattern = "index.html", negate = TRUE)) %>%
+    str_detect(value, pattern = "index.html", negate = TRUE)) %>%
+  dplyr::pull(value) %>%
   fs::file_delete()
 
 # 1. export the data to CSV from DB
@@ -15,7 +18,6 @@ source(here("R","apt_dshbd_get_data.R"), encoding = "UTF8")
 # 2. generate airport layout
 # UNCOMMENT  if new airports have been added
 # source(here("R","generate-apt-layout.R"), encoding = "UTF8")
-
 
 # 3. generate the various airport HTML pages
 source(here("R","apt_dshbd_render.R"), encoding = "UTF8")
