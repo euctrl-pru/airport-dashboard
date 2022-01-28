7be80358e9bc53a8885c964dcd790090d4942a46
Sam Peeters <sam.peeters@eurocontrol.int>
2022-01-28 14:35:11 +0100

    - All /docs files added to gitignore - Avg weekday hourly throughput figure creation changed to child RMD inapt_dshbd_render.Rmd - Factsheet creation added to apt_dshbd_render.R - Factsheet scripts changed to use Factsheet_layout_setup.R with layout variables

---
+++ R/Factsheet/Scripts/Factsheet_layout_setup.R
@@ -0,0 +1,15 @@
+
+# Lay-out setup for factsheets
+
+Page_width<<-450
+
+General_layout<<-c(0.4, 0.3, 0.3)
+
+Traffic_layout1<<-c(0.2, 0.5, 0.3)
+Traffic_height1<<-100
+
+Traffic_layout2<<-c(0.4, 0.6)
+Traffic_height2<<-100
+
+Traffic_layout3<<-1
+Traffic_height3<<-100
--- R/apt_dshbd_render.R
+++ R/apt_dshbd_render.R
@@ -11,19 +11,40 @@ source(here("R","apt_dshbd_create_df.R"), encoding = "UTF8")
 
 source(here("R","apt_dshbd_utils.R"), encoding = "UTF8")
 
+source(here("R", "Factsheet", "Scripts", "Factsheet_layout_setup.R"))
+
 # ---- CALL RENDER DASHBOARDS ----
 
 APT_DF <- APT_DF %>% arrange(AIRPORT) %>% mutate(idx = row_number())
 
 APT_DF %>%
-  # filter( AIRPORT %in% c("EBBR", "EGLL", "LATI")) %>%   # for debug
+  filter( AIRPORT %in% c("EBBR", "EGLL", "LATI")) %>%   # for debug
   pull(AIRPORT) %>%
   purrr::walk(
     .f = function(icao) {
       cat(paste0("==>", icao, "...\n"))
       rmarkdown::render(
-      input       = here("apt_dshbd_render.Rmd"),
-      params      = prepare_params(icao), 
-      output_file = here("docs", paste0(icao, ".html")))
+        input       = here("apt_dshbd_render.Rmd"),
+        params      = prepare_params(icao), 
+        output_file = here("docs", paste0(icao, ".html")))
       cat(paste0("==>", icao, "...end\n"))
-      })
+    })
+
+
+# Create factsheets
+
+# Recreate the country maps if necessary
+# source(here("R", "Factsheet", "Scripts", "Create_country_maps.R"), encoding = "UTF8")
+
+APT_DF %>%
+  filter( AIRPORT %in% c("EBBR", "EGLL", "LATI")) %>%   # for debug
+  pull(AIRPORT) %>%
+  purrr::walk(
+    .f = function(icao) {
+      cat(paste0("==>", icao, "...\n"))
+      rmarkdown::render(
+        input       = here("R", "Factsheet", "Scripts", "Factsheet_pdf.Rmd"),
+        params      = prepare_params(icao),
+        output_file = here("R", "Factsheet", "Factsheets", paste0("Factsheet_", icao, ".pdf")))
+      cat(paste0("==>", icao, "...end\n"))
+    })
