c67bd4b6384d6a376ae3258a343902edfb003ab2
Sam <sam.peeters@eurocontrol.int>
2022-06-09 08:28:42 +0000

    Test to generate dashboard

--- R/fig_traffic_variation_snapshot.R
+++ R/fig_traffic_variation_snapshot.R
@@ -1,6 +1,4 @@
 
-library(reshape2)
-library(patchwork)
 
 if (nrow(params$tfcvar) < 1) {
   cat("<p><b>Traffic evolution for this airport under development!</b>")
@@ -70,12 +68,15 @@ if (nrow(params$tfcvar) < 1) {
 
 # Factsheet figure
 
-Tfc_var1=melt(params$tfcvar, id.vars = "DAY", measure.vars = c("FLTS", "FLTS_2019", "MOV_AVG_WK")) %>% 
-  mutate(variable2 = ifelse(variable=="FLTS", 
-                           "Flights", 
-                           ifelse(variable=="FLTS_2019", 
-                                  "Flights 2019 (Reference)", 
-                                  ifelse(variable=="MOV_AVG_WK", 
+library(reshape2)
+library(patchwork)
+
+Tfc_var1=melt(params$tfcvar, id.vars = "DAY", measure.vars = c("FLTS", "FLTS_2019", "MOV_AVG_WK")) %>%
+  mutate(variable2 = ifelse(variable=="FLTS",
+                           "Flights",
+                           ifelse(variable=="FLTS_2019",
+                                  "Flights 2019 (Reference)",
+                                  ifelse(variable=="MOV_AVG_WK",
                                          "% vs 2019\n(7-day Moving Average)",
                                          variable))))
 
@@ -88,14 +89,14 @@ Tfc_var_fig1 = ggplot(data=filter(Tfc_var1, variable %in% c("FLTS", "FLTS_2019")
         axis.ticks.x=element_blank()) +
   labs(x="", y="")
 Tfc_var_fig2 = ggplot(data=filter(Tfc_var1, variable == "MOV_AVG_WK")) +
-  geom_line(aes(x=DAY, y=value, linetype='% vs 2019\n(7-day Moving Average)', group=variable2), 
-            colour="green", size=linesize_factsheet)+ 
-  scale_y_continuous(labels = scales::percent, breaks = seq(-2, 2, by = 0.2)) + 
+  geom_line(aes(x=DAY, y=value, linetype='% vs 2019\n(7-day Moving Average)', group=variable2),
+            colour="green", size=linesize_factsheet)+
+  scale_y_continuous(labels = scales::percent, breaks = seq(-2, 2, by = 0.2)) +
   scale_x_date(date_labels = "%b %Y", breaks = seq(as.Date("2020-04-01"), max(Tfc_var1$DAY), "3 months")) +
   theme_factsheet() +
   theme(legend.position = "right") +
   labs(x="", y="")
 Tfc_var_fig=Tfc_var_fig1/Tfc_var_fig2 + plot_layout(guides = "collect")
-ggsave(here("media", "factsheet", paste0("Tfc_var_snapshot_", params$icao, ".png")), plot=Tfc_var_fig, 
+ggsave(here("media", "factsheet", paste0("Tfc_var_snapshot_", params$icao, ".png")), plot=Tfc_var_fig,
        width = Traffic_layout2[2]*Page_width, height = Traffic_height2, units = "cm", dpi=100, limitsize = FALSE)
 
