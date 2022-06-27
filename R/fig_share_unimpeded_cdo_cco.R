7f6117e1d77d82cccef6fb371203f68e9ac5b1d8
Sam <sam.peeters@eurocontrol.int>
2022-06-27 11:49:20 +0200

    Isuue with markers fixed for Median CDO-CCO altitude and Share of unimpeded flights

--- R/fig_share_unimpeded_cdo_cco.R
+++ R/fig_share_unimpeded_cdo_cco.R
@@ -28,7 +28,9 @@ button_type_list <- list(
   yanchor   = "bottom"
 )
 
-plot_ly(SHARE_CDO_CCO_FLIGHTS_PLOT, 
+if (nrow(filter(AVG_TIME_LVL_PLOT, YEAR==max_year))>1) {
+  
+  share_cdo_cdo_fig = plot_ly(SHARE_CDO_CCO_FLIGHTS_PLOT, 
         x = ~MONTH_NUM, 
         y = ~SHARE_CDO_FLIGHTS*100, 
         customdata = ~YEAR, 
@@ -50,7 +52,49 @@ plot_ly(SHARE_CDO_CCO_FLIGHTS_PLOT,
             name = 'Descent (Noise CDO)') %>% 
   add_trace(y = ~SHARE_CCO_FLIGHTS_BLW_100*100, 
             line = list(color = 'rgb(106, 168, 82)', dash = 'dash'),
-            name = 'Climb (Noise CCO)') %>% 
+            name = 'Climb (Noise CCO)')
+  
+} else {
+  
+  share_cdo_cdo_fig = plot_ly(SHARE_CDO_CCO_FLIGHTS_PLOT, 
+                              x = ~MONTH_NUM, 
+                              y = ~SHARE_CDO_FLIGHTS*100, 
+                              customdata = ~YEAR, 
+                              line = list(color = 'rgb(40, 120, 181)'),
+                              marker = list(color = 'rgb(40, 120, 181)'),
+                              symbol = ~max_year,
+                              symbols = 'circle',
+                              hovertemplate = "%{y:.1f}%",
+                              type = 'scatter',
+                              mode = 'lines', 
+                              name = 'Descent (Fuel CDO)',
+                              transforms = list(list(type      = 'filter',
+                                                     target    = "customdata",
+                                                     operation = '=',
+                                                     value     = max_year)
+                              )) %>% 
+    add_trace(y = ~SHARE_CCO_FLIGHTS*100, 
+              line = list(color = 'rgb(106, 168, 82)'),
+              marker = list(color = 'rgb(106, 168, 82)'),
+              symbol = ~max_year,
+              symbols = 'circle',
+              name = 'Climb (Fuel CCO)') %>%
+    add_trace(y = ~SHARE_CDO_FLIGHTS_BLW_70*100, 
+              line = list(color = 'rgb(40, 120, 181)', dash = 'dash'),
+              marker = list(color = 'rgb(40, 120, 181)'),
+              symbol = ~max_year+1,
+              symbols = 'triangle-up',
+              name = 'Descent (Noise CDO)') %>% 
+    add_trace(y = ~SHARE_CCO_FLIGHTS_BLW_100*100, 
+              line = list(color = 'rgb(106, 168, 82)', dash = 'dash'),
+              marker = list(color = 'rgb(106, 168, 82)'),
+              symbol = ~max_year+1,
+              symbols = 'triangle-up',
+              name = 'Climb (Noise CCO)')
+  
+}
+
+share_cdo_cdo_fig = share_cdo_cdo_fig  %>% 
   layout(barmode = 'group',
          hovermode   = "x unified",
          xaxis       = tick_yr_no_title,
@@ -79,7 +123,7 @@ plot_ly(SHARE_CDO_CCO_FLIGHTS_PLOT,
   config( displaylogo = FALSE,
           modeBarButtonsToRemove = config_bar_remove_buttons)
 
-
+share_cdo_cdo_fig
 
 
 
