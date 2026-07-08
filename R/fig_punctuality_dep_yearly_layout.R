24b89665811f12a2b89fbec6a36236c9b4dcc008
tdlange <75446106+tdlange@users.noreply.github.com>
2026-07-08 10:32:29 +0200

    Update fig_punctuality_dep_yearly_layout.R
    modification of the category naming

--- R/fig_punctuality_dep_yearly_layout.R
+++ R/fig_punctuality_dep_yearly_layout.R
@@ -1,40 +1,40 @@
 
 pun_col <- c(
-  "#1f4e79", # EARLY >30		#rgb 31,78,121
+  "#1f4e79", # EARLY >=31 	#rgb 31,78,121
   "#8497b0", # EARLY 16-30	#rgb 132,151,176
   "#c5e0b4", # EARLY 5-15	  #rgb 197,224,180
   "#70ad47", # ON TIME -+4	#rgb 112,173,71
   "#ffe699", # LATE 5-15		#rgb 255,230,153
   "#e1c000", # LATE 16-30	  #rgb 225,192,0
   "#ff0000", # LATE 31-60	  #rgb 255,0,0
-  "#c00000"  # LATE >60		  #rgb 192,0,0
+  "#c00000"  # LATE >=61	  #rgb 192,0,0
 )           
 
 punc_dep_yy_plot <- params$punc_dep_yy
 
 #punc_dep_yy_plot$PUNCT_CAT <- factor(punc_dep_yy_plot$PUNCT_CAT,
 #                                     levels = c(
-#                                       "Early >30",
+#                                       "Early >=31",
 #                                       "Early 16-30",
 #                                       "Early 5-15",
 #                                       "On Time -+4",
 #                                       "Late 5-15",
 #                                       "Late 16-30",
 #                                       "Late 31-60",
-#                                       "Late >60"
+#                                       "Late >=61"
 #                                     )
 #)
 
 punc_dep_yy_plot$PUNCT_CAT <- factor(punc_dep_yy_plot$PUNCT_CAT,
                                      levels = c(
-                                       "[<-30]",
+                                       "[<=-31]",
                                        "[-16, -30]",
                                        "[- 5, -15]",
                                        "[- 4, + 4]",
                                        "[+ 5, +15]",
                                        "[+16, +30]",
                                        "[+31, +60]",
-                                       "[>+60]"
+                                       "[>=+61]"
                                      )
 )
 
